d405c68b904956dce7fca68a13c5982bc7d20c6c
Mustafizur Rahman <mustafizur@bs-23.net>
2019-03-14 13:28:19 +0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app';
+  constructor() {
+
+
+
+  }
+
+  listner() {
+    window.addEventListener(
+      'storage',
+      event => {
+        console.log(event.key);
+        const credentials = JSON.parse(sessionStorage.getItem('CREDENTIALS_TOKEN'));
+        if (event.key === 'REQUESTING_SHARED_CREDENTIALS' && credentials) {
+          console.log('localStorage set ');
+          localStorage.setItem('CREDENTIALS_SHARING', JSON.stringify({ token: 'any-token-you-want' + Date.now().toString() }));
+          localStorage.removeItem('CREDENTIALS_SHARING');
+        }
+        if (event.key === 'CREDENTIALS_SHARING' && !credentials) {
+          console.log('sessionStorage set ');
+          sessionStorage.setItem('CREDENTIALS_TOKEN', event.newValue);
+        }
+        if (event.key === 'CREDENTIALS_FLUSH' && credentials) {
+          sessionStorage.removeItem('CREDENTIALS_TOKEN');
+        }
+      },
+      false
+    );
+
+
+
+
+  }
+
+  ngOnInit(): void {
+    this.listner();
+    localStorage.setItem('REQUESTING_SHARED_CREDENTIALS', Date.now().toString());
+    localStorage.removeItem('REQUESTING_SHARED_CREDENTIALS');
+  }
+  logOut() {
+    localStorage.setItem('CREDENTIALS_FLUSH', Date.now().toString());
+    //window.localStorage.removeItem('CREDENTIALS_FLUSH');
+    localStorage.removeItem('CREDENTIALS_FLUSH');
+  }
+  tokenGenerate() {
+    sessionStorage.setItem('CREDENTIALS_TOKEN', JSON.stringify({ token: 'any-token-you-want' + Date.now().toString() }));
+  }
 }
